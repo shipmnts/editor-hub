0fcbbda8763029eb27b6f9ad7969ae5e9c9f2e20
devcodes9 <devdalia9@gmail.com>
2024-10-15 23:22:07 +0530

    tweaked some changes

--- RichTextEditorWrapper/RichTextEditorWrapper.jsx
+++ RichTextEditorWrapper/RichTextEditorWrapper.jsx
@@ -42,6 +42,7 @@ const RichTextEditorWrapper = (props) => {
     onSubmit,
     mentionChars = ["@"],
     showDenotationChar = true,
+    theme="snow"
   } = props;
 
   const { quill, quillRef, Quill } = useQuill({
@@ -97,7 +98,7 @@ const RichTextEditorWrapper = (props) => {
       "emoji-textarea": false,
       "emoji-shortname": true,
     },
-    theme: props?.theme,
+    theme,
   });
   if (Quill && !quill) {
     try {
